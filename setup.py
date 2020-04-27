f6626e532afa4e3dfd84c39745f61987b7eeb995
Rajat Singh <iamsinghrajat@gmail.com>
2020-04-27 13:30:53 +0530

    Fix: setup changes

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="async-lru-iamsinghrajat", # Replace with your own username
-    version="0.0.1",
+    name="async-cache", # Replace with your own username
+    version="0.0.2",
     author="Rajat Singh",
     author_email="iamsinghrajat@gmail.com",
-    description="An asyncio LRU Cache",
+    description="An asyncio Cache",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/iamsinghrajat/AsyncioLRU",
