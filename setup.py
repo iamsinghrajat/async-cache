4e7a434f1ec94f382793608f1cbd8d3c766fdea8
Rajat Singh <iamsinghrajat@gmail.com>
2020-04-27 16:43:03 +0530

    Fix: added travis CI

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 import setuptools
+import os
+import io
 
-with open("README.rst", "r") as fh:
-    long_description = fh.read()
+
+def read(*parts):
+    filename = os.path.join(os.path.abspath(os.path.dirname(__file__)), *parts)
+
+    with io.open(filename, encoding='utf-8', mode='rt') as fp:
+        return fp.read()
 
 
 setuptools.setup(
     name="async-cache",
-    version="0.0.4",
+    version="0.0.5",
     author="Rajat Singh",
     author_email="iamsinghrajat@gmail.com",
-    description="An asyncio Cache",
-    long_description=long_description,
+    description=read("README.rst"),
     long_description_content_type="text/markdown",
     url="https://github.com/iamsinghrajat/async-cache",
     packages=setuptools.find_packages(),
