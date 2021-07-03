7040ae0e162611d9fbeecc6f0c47d8ba4eacd869
Rajat Singh <rajat.singh@mindtickle.com>
2021-07-03 23:46:49 +0530

    Fix: added skip arg in TTL cache

--- cache/async_ttl.py
+++ cache/async_ttl.py
@@ -7,7 +7,11 @@ class AsyncTTL:
     class _TTL(LRU):
         def __init__(self, time_to_live, maxsize):
             super().__init__(maxsize=maxsize)
-            self.time_to_live = datetime.timedelta(seconds=time_to_live)
+
+            self.time_to_live = datetime.timedelta(
+                seconds=time_to_live
+            ) if time_to_live else None
+
             self.maxsize = maxsize
 
         def __contains__(self, key):
@@ -15,7 +19,7 @@ class AsyncTTL:
                 return False
             else:
                 key_expiration = super().__getitem__(key)[1]
-                if key_expiration < datetime.datetime.now():
+                if key_expiration and key_expiration < datetime.datetime.now():
                     del self[key]
                     return False
                 else:
@@ -26,18 +30,26 @@ class AsyncTTL:
             return value
 
         def __setitem__(self, key, value):
-            ttl_value = datetime.datetime.now() + self.time_to_live
+            ttl_value = (
+                    datetime.datetime.now() + self.time_to_live
+            ) if self.time_to_live else None
             super().__setitem__(key, (value, ttl_value))
 
-    def __init__(self, time_to_live=60, maxsize=1024):
+    def __init__(
+            self, time_to_live=60, maxsize=1024, skip_args: int = 0
+    ):
         """
+
+        :param time_to_live: Use time_to_live as None for non expiring cache
         :param maxsize: Use maxsize as None for unlimited size cache
+        :param skip_args: Use `1` to skip first arg of func in determining cache key
         """
         self.ttl = self._TTL(time_to_live=time_to_live, maxsize=maxsize)
+        self.skip_args = skip_args
 
     def __call__(self, func):
         async def wrapper(*args, **kwargs):
-            key = KEY(args, kwargs)
+            key = KEY(args[self.skip_args:], kwargs)
             if key in self.ttl:
                 val = self.ttl[key]
             else:
--- tests/lru_test.py
+++ tests/lru_test.py
@@ -1,18 +1,77 @@
-from cache import AsyncLRU
+from cache import AsyncLRU, AsyncTTL
 import asyncio
 import time
 
 
 @AsyncLRU(maxsize=128)
-async def fn(wait: int):
+async def func(wait: int):
     await asyncio.sleep(wait)
+
+
+class TestClassFunc:
+    @AsyncLRU(maxsize=128)
+    async def obj_func(self, wait: int):
+        await asyncio.sleep(wait)
+
+    @staticmethod
+    @AsyncTTL(maxsize=128, time_to_live=None, skip_args=1)
+    async def skip_arg_func(arg: int, wait: int):
+        await asyncio.sleep(wait)
+
+    @classmethod
+    @AsyncLRU(maxsize=128)
+    async def class_func(cls, wait: int):
+        await asyncio.sleep(wait)
 
 
 def test():
     t1 = time.time()
-    asyncio.get_event_loop().run_until_complete(fn(4))
+    asyncio.get_event_loop().run_until_complete(func(4))
     t2 = time.time()
-    asyncio.get_event_loop().run_until_complete(fn(4))
+    asyncio.get_event_loop().run_until_complete(func(4))
+    t3 = time.time()
+    t_first_exec = (t2 - t1) * 1000
+    t_second_exec = (t3 - t2) * 1000
+    print(t_first_exec)
+    print(t_second_exec)
+    assert t_first_exec > 4000
+    assert t_second_exec < 4000
+
+
+def test_obj_fn():
+    t1 = time.time()
+    obj = TestClassFunc()
+    asyncio.get_event_loop().run_until_complete(obj.obj_func(4))
+    t2 = time.time()
+    asyncio.get_event_loop().run_until_complete(obj.obj_func(4))
+    t3 = time.time()
+    t_first_exec = (t2 - t1) * 1000
+    t_second_exec = (t3 - t2) * 1000
+    print(t_first_exec)
+    print(t_second_exec)
+    assert t_first_exec > 4000
+    assert t_second_exec < 4000
+
+
+def test_class_fn():
+    t1 = time.time()
+    asyncio.get_event_loop().run_until_complete(TestClassFunc.class_func(4))
+    t2 = time.time()
+    asyncio.get_event_loop().run_until_complete(TestClassFunc.class_func(4))
+    t3 = time.time()
+    t_first_exec = (t2 - t1) * 1000
+    t_second_exec = (t3 - t2) * 1000
+    print(t_first_exec)
+    print(t_second_exec)
+    assert t_first_exec > 4000
+    assert t_second_exec < 4000
+
+
+def test_skip_args():
+    t1 = time.time()
+    asyncio.get_event_loop().run_until_complete(TestClassFunc.skip_arg_func(5, 4))
+    t2 = time.time()
+    asyncio.get_event_loop().run_until_complete(TestClassFunc.skip_arg_func(6, 4))
     t3 = time.time()
     t_first_exec = (t2 - t1) * 1000
     t_second_exec = (t3 - t2) * 1000
@@ -24,3 +83,6 @@ def test():
 
 if __name__ == "__main__":
     test()
+    test_obj_fn()
+    test_class_fn()
+    test_skip_args()
